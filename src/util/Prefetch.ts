94a6aac85d54cb911278f356d516e8dc082cbe54
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2022-09-05 22:26:01 +0200

    API: Make `Prefetch` respect `CollectionBlock` limit.

--- src/util/Prefetch.ts
+++ src/util/Prefetch.ts
@@ -3,10 +3,7 @@ import { PageModel } from '../models/PageModel';
 import { ProductsApi } from '../api/product';
 import { VendorsApi } from '../api/vendor';
 
-const Prefetch = (
-    page: PageModel,
-    query: any
-) => {
+const Prefetch = (page: PageModel, query: any) => {
     return new Promise<{
         collections?: any;
         products?: any;
@@ -30,8 +27,13 @@ const Prefetch = (
             switch (type) {
                 case 'CollectionBlock':
                 case 'collection':
-                    if (handle && !process.browser)
+                    if (handle && !process.browser) {
                         collections[handle] = await CollectionApi(handle);
+                        if (slice?.primary?.limit > 0)
+                            collections[handle].items = collections[
+                                handle
+                            ].items.slice(0, slice?.primary?.limit);
+                    }
                     break;
                 case 'ContentBlock':
                     const res = await Prefetch(
