b79a2dfe47b63258e65e3e5e5f4f6f0b68401b98
Filiph Siitam Sandström <filiph.sandstrom@filfatstudios.com>
2022-09-24 13:24:38 +0200

    chore(SEO): Fix SEO titles.

--- nextseo.config.js
+++ nextseo.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
     // FIXME: Name should be per-store not hardcoded
-    titleTemplate: `%s | ${process.env.DOMAIN === 'candybysweden.com' ? 'Candy by Sweden' : 'happysnus'}`
+    titleTemplate: `%s - ${process.env.DOMAIN === 'candybysweden.com' ? 'Candy by Sweden' : 'happysnus'}`
 };
