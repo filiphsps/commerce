723a294a2133f4d56cab55ebbff4b8435a08a423
Filiph Siitam Sandström <filiph.sandstrom@filfatstudios.com>
2023-05-16 00:17:27 +0200

    fix(document): Fix build error.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -41,17 +41,17 @@ class App extends Document {
                     <link
                         rel="preconnect"
                         href="https://fonts.gstatic.com"
-                        crossOrigin="true"
+                        crossOrigin=""
                     />
                     <link
                         rel="preconnect"
                         href="https://cdn.shopify.com"
-                        crossOrigin="true"
+                        crossOrigin=""
                     />
                     <link
                         rel="preconnect"
-                        href="https://candybysweden.myshopify.com/"
-                        crossOrigin="true"
+                        href="https://sweet-side-of-sweden.myshopify.com/"
+                        crossOrigin=""
                     />
                     <link
                         href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap"
