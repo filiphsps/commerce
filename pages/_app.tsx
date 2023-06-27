1f5e624c4c9876c0c922982a35dc23a876c073fd
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2023-06-27 07:57:23 +0200

    SEO: Remove SearchBox from jsonld.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -51,7 +51,10 @@ const sendPageView = (analyticsPageData: any, locale: string = Config.i18n.local
             payload
         },
         // TODO: do this properly
-        `checkout.${Config.domain.replace('www.', '').replace('preview.', '').replace('staging.', '')}`
+        `checkout.${Config.domain
+            .replace('www.', '')
+            .replace('preview.', '')
+            .replace('staging.', '')}`
     );
 };
 
@@ -60,11 +63,14 @@ const StoreApp = withStore(
         const router = useRouter();
         useShopifyCookies({
             hasUserConsent: true,
-            domain: (process.env.NODE_ENV !== 'development' && `${Config.domain}`
-                .replace('checkout.', '')
-                .replace('www.', '')
-                .replace('preview.', '')
-                .replace('staging.', '')) || undefined
+            domain:
+                (process.env.NODE_ENV !== 'development' &&
+                    `${Config.domain}`
+                        .replace('checkout.', '')
+                        .replace('www.', '')
+                        .replace('preview.', '')
+                        .replace('staging.', '')) ||
+                undefined
         });
 
         const country = (
@@ -74,30 +80,34 @@ const StoreApp = withStore(
             locale?.split('-')[0] || Config.i18n.locales[0].split('-')[0]
         ).toUpperCase() as LanguageCode;
 
-        const { data: store } = useSWR([`store`], () => StoreApi({ locale: router.locale }) as any, {
-            fallbackData: {
-                // FIXME: Use CMS for these
-                name: 'Sweet Side of Sweden',
-                currency: 'USD',
-                logo: {
-                    src: 'https://cdn.shopify.com/s/files/1/0761/8848/3889/files/logo_d38724cd-5589-4b7a-9f61-62c274f52720.png?v=1686651235'
-                },
-                favicon: {
-                    src: 'https://cdn.shopify.com/s/files/1/0761/8848/3889/files/favicon.png?v=1686651228'
-                },
-                accent: {
-                    primary: Config.colors.primary,
-                    secondary: Config.colors.secondary
-                },
-                color: {
-                    primary: '#ffffff',
-                    secondary: '#ffffff'
-                },
-                block: {
-                    border_radius: '0.25rem'
+        const { data: store } = useSWR(
+            [`store`],
+            () => StoreApi({ locale: router.locale }) as any,
+            {
+                fallbackData: {
+                    // FIXME: Use CMS for these
+                    name: 'Sweet Side of Sweden',
+                    currency: 'USD',
+                    logo: {
+                        src: 'https://cdn.shopify.com/s/files/1/0761/8848/3889/files/logo_d38724cd-5589-4b7a-9f61-62c274f52720.png?v=1686651235'
+                    },
+                    favicon: {
+                        src: 'https://cdn.shopify.com/s/files/1/0761/8848/3889/files/favicon.png?v=1686651228'
+                    },
+                    accent: {
+                        primary: Config.colors.primary,
+                        secondary: Config.colors.secondary
+                    },
+                    color: {
+                        primary: '#ffffff',
+                        secondary: '#ffffff'
+                    },
+                    block: {
+                        border_radius: '0.25rem'
+                    }
                 }
             }
-        });
+        );
 
         const analyticsShopData = {
             shopId: `gid://shopify/Shop/${Config.shopify.shop_id}`,
@@ -157,22 +167,22 @@ const StoreApp = withStore(
                             --color-sale: #d91e18;
                             --accent-primary: ${Color(store.accent.primary).hex().toString()};
                             --accent-primary-dark: ${Color(store.accent.primary)
-                            .darken(0.25)
-                            .hex()
-                            .toString()};
+                                .darken(0.25)
+                                .hex()
+                                .toString()};
                             --accent-primary-light: ${Color(store.accent.primary)
-                            .lighten(0.45)
-                            .hex()
-                            .toString()};
+                                .lighten(0.45)
+                                .hex()
+                                .toString()};
                             --accent-secondary: ${Color(store.accent.secondary).hex().toString()};
                             --accent-secondary-dark: ${Color(store.accent.secondary)
-                            .darken(0.25)
-                            .hex()
-                            .toString()};
+                                .darken(0.25)
+                                .hex()
+                                .toString()};
                             --accent-secondary-light: ${Color(store.accent.secondary)
-                            .lighten(0.25)
-                            .hex()
-                            .toString()};
+                                .lighten(0.25)
+                                .hex()
+                                .toString()};
                             --block-border-radius: ${store.block.border_radius};
                             background: var(--accent-primary);
                         }
@@ -180,22 +190,14 @@ const StoreApp = withStore(
                     {/* eslint-enable indent */}
                 </Head>
 
-                <SiteLinksSearchBoxJsonLd
-                    url={`https://${Config.domain}/`}
-                    potentialActions={[
-                        {
-                            target: `https://${Config.domain}/search/?q`,
-                            queryInput: 'search_term_string'
-                        }
-                    ]}
-                />
+                {/* TODO: Get this dynamically */}
                 <SocialProfileJsonLd
                     type="Organization"
                     name={store.name}
                     url={`https://${Config.domain}/`}
                     sameAs={[
                         'https://instagram.com/sweetsideofsweden',
-                        'https://twitter.com/sweetsideofsweden'
+                        'https://facebook.com/sweetsideofsweden'
                     ]}
                 />
 
@@ -214,7 +216,7 @@ const StoreApp = withStore(
                             <Component
                                 key={router.asPath}
                                 {...pageProps}
-                                { ...analytics}
+                                {...analytics}
                                 store={store}
                             />
                         </PageProvider>
@@ -236,7 +238,7 @@ const StoreApp = withStore(
         }
     },
     {
-        listener: () => { },
+        listener: () => {},
         logging: false
     }
 );
