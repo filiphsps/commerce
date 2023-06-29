569a11637ec9bcf86f8ff3ae68d34d32eef417fe
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2023-06-29 19:48:50 +0200

    API: Misc fixes.

--- scripts/useclient-directive-hack.mjs
+++ scripts/useclient-directive-hack.mjs
@@ -1,32 +1,40 @@
-// FIXME: Remove this extremely ugly hack once @@shopify/hydrogen-react or @next fixes their library.
-
+/* eslint-disable no-console */
 import fs from 'fs';
-
-const files = [
-    'AddToCartButton',
-    'BuyNowButton',
-    'CartCheckoutButton',
-    'CartLineProvider',
-    'CartProvider',
-    'load-script',
-    'ModelViewer',
-    'ProductProvider',
-    'ShopifyProvider',
-    'useShopifyCookies'
-];
 
 try {
     const payload = `'use client';\n\n`;
-    const prefix = 'node_modules/@shopify/hydrogen-react/dist/';
 
-    files.map((file) => {
-        [`${prefix}/node-dev/${file}.mjs`, `${prefix}/node-prod/${file}.mjs`].map((path) => {
-            const data = fs.readFileSync(path).toString();
-            if (data.startsWith(payload)) return;
+    {
+        // TODO: Remove this extremely ugly hack once @shopify/hydrogen-react or @next fixes their library.
+        const files = [
+            'AddToCartButton',
+            'BuyNowButton',
+            'CartCheckoutButton',
+            'CartLineProvider',
+            'CartProvider',
+            'load-script',
+            'ModelViewer',
+            'ProductProvider',
+            'ShopifyProvider',
+            'useShopifyCookies'
+        ];
 
-            fs.writeFileSync(path, `${payload}${data}`);
+        const prefix = 'node_modules/@shopify/hydrogen-react/dist/';
+
+        files.map((file) => {
+            [
+                `${prefix}/node-dev/${file}.mjs`,
+                `${prefix}/node-prod/${file}.mjs`,
+                `${prefix}/browser-dev/${file}.mjs`,
+                `${prefix}/browser-prod/${file}.mjs`
+            ].map((path) => {
+                const data = fs.readFileSync(path).toString();
+                if (data.startsWith(payload)) return;
+
+                fs.writeFileSync(path, `${payload}${data}`);
+            });
         });
-    });
+    }
 } catch (e) {
     console.log(e);
     console.log('Hack failed');
