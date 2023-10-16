d851479d6c03940ba08ec7114f9c93b5ebf420e6
Filiph Siitam Sandström <filiph@nordcom.io>
2023-10-16 11:03:51 +0200

    Meta: Remove old `ipv4` workaround.

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 import createNextPluginPreval from '@sweetsideofsweden/next-plugin-preval/config.js';
-import dns from 'node:dns';
 import { i18n } from './next-i18next.config.cjs';
 import manifest from './package.json' assert { type: 'json' };
-
-// See https://github.com/vercel/next.js/issues/44062#issuecomment-1445185361
-dns.setDefaultResultOrder('ipv4first');
 
 const withNextPluginPreval = createNextPluginPreval();
 
