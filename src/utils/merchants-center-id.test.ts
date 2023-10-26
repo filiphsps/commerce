1e68e5f05edfb3827996c8e357e8981c35768e05
Filiph Siitam Sandström <filiph@nordcom.io>
2023-10-27 01:13:52 +0200

    CI: Replace `jest` with `vitest`.

--- src/utils/merchants-center-id.test.ts
+++ src/utils/merchants-center-id.test.ts
@@ -1,14 +1,13 @@
 import { NextLocaleToLocale } from '@/utils/locale';
 import { ProductToMerchantsCenterId } from '@/utils/merchants-center-id';
-import type { ShopifyAnalyticsProduct } from '@shopify/hydrogen-react';
 
 describe('Utils', () => {
     describe('MerchantsCenterId', () => {
         it(`should generate a Merchants Center product ID based on the product's Shopify GIDs`, () => {
-            const product: ShopifyAnalyticsProduct = {
+            const product = {
                 productGid: 'gid://shopify/Product/123',
                 variantGid: 'gid://shopify/ProductVariant/456'
-            } as ShopifyAnalyticsProduct;
+            } as any;
 
             const locale = NextLocaleToLocale('en-US');
 
@@ -18,13 +17,14 @@ describe('Utils', () => {
             });
 
             expect(merchantsCenterId).toBe('shopify_US_123_456');
+            expect(merchantsCenterId).toMatchSnapshot();
         });
 
         it(`should generate a Merchants Center product ID based on the product's Shopify GIDs`, () => {
-            const product: ShopifyAnalyticsProduct = {
+            const product = {
                 productGid: 'gid://shopify/Product/123',
                 variantGid: 'gid://shopify/ProductVariant/456'
-            } as ShopifyAnalyticsProduct;
+            } as any;
 
             const locale = NextLocaleToLocale('fr-CA');
 
@@ -34,6 +34,7 @@ describe('Utils', () => {
             });
 
             expect(merchantsCenterId).toBe('shopify_CA_123_456');
+            expect(merchantsCenterId).toMatchSnapshot();
         });
     });
 });
