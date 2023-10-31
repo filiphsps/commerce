a4f9bbf8128d7ba89e04acbd78d25daa5f807f97
Filiph Siitam Sandström <filiph@nordcom.io>
2023-10-31 20:00:24 +0000

    Locale: Handle missing locale.
    This commits redirects the user to the locale configured as the default
    and therefore resolves FRO-45 entirely.
    
    FIXME: It does not yet account for the user's location nor his/her
    browser preferred language. That is an urgent TODO before release.

--- src/app/slice-simulator/layout.tsx
+++ src/app/slice-simulator/layout.tsx
@@ -1,3 +1,12 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    robots: {
+        follow: true,
+        index: false
+    }
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
