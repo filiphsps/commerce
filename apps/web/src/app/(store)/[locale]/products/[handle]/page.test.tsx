30067b450549b3288ba968863a82ccf9f153e7a1
Filiph Siitam Sandström <filiph@nordcom.io>
2023-11-04 21:37:16 +0100

    Tests: Introduce page testing by adding some tests for the product page.
    We still need to add a whole lot more tests, but this is an amazing
    start. There are still som quirks that needs to be figured out, but
    overall this seems to be the right direction for testing `app router`
    pages.

---
+++ apps/web/src/app/(store)/[locale]/products/[handle]/page.test.tsx
@@ -0,0 +1,100 @@
+import ProductPage, { generateMetadata } from './page';
+import { describe, expect, it, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+
+import type { ProductPageParams } from './page';
+
+vi.mock('@/utils/prefetch', () => ({
+    Prefetch: vi.fn().mockResolvedValue({})
+}));
+vi.mock('@/i18n/dictionary', () => ({
+    getDictionary: vi.fn().mockResolvedValue({})
+}));
+
+describe('Pages', () => {
+    // Mock the `StorefrontApiClient` function to prevent API calls.
+    vi.mock('@/api/shopify', () => ({
+        StorefrontApiClient: vi.fn().mockReturnValue({
+            query: vi.fn().mockResolvedValue({})
+        })
+    }));
+
+    // Mock the `PageApi` function to prevent API calls.
+    vi.mock('@/api/page', () => ({
+        PageApi: vi.fn().mockResolvedValue({
+            page: {
+                slices: []
+            }
+        })
+    }));
+
+    describe('ProductPage', () => {
+        const { product } = vi.hoisted(() => ({
+            product: {
+                title: 'Test Product',
+                handle: 'test-product',
+                vendor: 'Test Vendor',
+                images: {
+                    edges: [
+                        {
+                            node: {
+                                id: '1',
+                                url: 'https://placehold.co/50x50.png'
+                            }
+                        }
+                    ]
+                },
+                variants: {
+                    edges: [
+                        {
+                            node: {
+                                price: {
+                                    amount: '10.00',
+                                    currencyCode: 'USD'
+                                },
+                                compareAtPrice: {
+                                    amount: '15.00',
+                                    currencyCode: 'USD'
+                                }
+                            }
+                        }
+                    ]
+                }
+            }
+        }));
+        const params: ProductPageParams = { locale: 'en-US', handle: product.handle };
+
+        // Mock the `ProductApi` function to prevent API calls.
+        vi.mock('@/api/shopify/product', () => ({
+            ProductApi: vi.fn().mockResolvedValue({ ...product })
+        }));
+
+        it('renders the product title and vendor', async () => {
+            render(await ProductPage({ params }));
+            const title = await screen.findByText(product.title);
+            const vendor = await screen.findByText(product.vendor);
+
+            expect(title).toBeInTheDocument();
+            expect(vendor).toBeInTheDocument();
+        });
+
+        it.skip('renders the product image', async () => {
+            render(await ProductPage({ params }));
+            const image = await screen.findByRole('img');
+            expect(image).toHaveAttribute('src', product.images.edges[0].node.url);
+        });
+
+        it.skip('renders the product pricing', async () => {
+            render(await ProductPage({ params }));
+            const price = await screen.findByText('$10.00');
+            const compareAtPrice = await screen.findByText('$15.00');
+            expect(price).toBeInTheDocument();
+            expect(compareAtPrice).toBeInTheDocument();
+        });
+
+        it('generates the correct metadata', async () => {
+            const metadata = await generateMetadata({ params });
+            expect(metadata!.title).toBe(`${product.vendor} ${product.title}`);
+        });
+    });
+});
