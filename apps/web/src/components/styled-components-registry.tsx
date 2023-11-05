ba82aa7e12e007e9bcbdb6868e087df1a503b7c3
Filiph Siitam Sandström <filiph@nordcom.io>
2023-11-05 17:43:40 +0100

    Utils: Add `RemoveInvalidProps` utility to filter invalid props.
    The use case for this utility is when you want to pass forward props to
    a child component, but you don't want to pass invalid props to it;
    e.g. instead of doing `<Comp {...props}/>` you now sanitize it with
    `RemoveInvalidProps` like this `<Comp {...RemoveInvalidProps(props)}/>`.

--- apps/web/src/components/styled-components-registry.tsx
+++ apps/web/src/components/styled-components-registry.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from 'react';
 import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
 
+import isPropValid from '@emotion/is-prop-valid';
 import { useServerInsertedHTML } from 'next/navigation';
 
 export default function StyledComponentsRegistry({ children }: { children: React.ReactNode }) {
@@ -18,5 +19,9 @@ export default function StyledComponentsRegistry({ children }: { children: React
 
     if (typeof window !== 'undefined') return <>{children}</>;
 
-    return <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>{children}</StyleSheetManager>;
+    return (
+        <StyleSheetManager sheet={styledComponentsStyleSheet.instance} shouldForwardProp={isPropValid}>
+            {children}
+        </StyleSheetManager>
+    );
 }
