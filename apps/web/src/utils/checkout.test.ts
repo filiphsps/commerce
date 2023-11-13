a593ed54be56f0fc50447c1bf6b4542ac2f5c2c4
Filiph Siitam Sandström <filiph@nordcom.io>
2023-11-13 20:36:33 +0100

    Errors: Move to `utils` folder.

---
+++ apps/web/src/utils/checkout.test.ts
@@ -0,0 +1,165 @@
+import { Checkout } from '@/utils/checkout';
+import type { Locale } from '@/utils/locale';
+import type { CartWithActions } from '@shopify/hydrogen-react';
+import { describe, expect, it, vi } from 'vitest';
+
+describe('utils', () => {
+    describe('checkout', () => {
+        vi.mock('@/utils/build-config', () => ({
+            BuildConfig: {
+                shopify: {
+                    domain: 'example.com',
+                    checkout_domain: 'checkout.example.com'
+                }
+            }
+        }));
+
+        vi.mock('@/utils/pluralize', () => ({
+            Pluralize: vi.fn(({ count, noun, suffix }) => `${count} ${noun}${count > 1 ? suffix : ''}`)
+        }));
+
+        vi.mock('@/utils/merchants-center-id', () => ({
+            ProductToMerchantsCenterId: vi.fn(() => 'product-id')
+        }));
+
+        const cart: CartWithActions = {
+            totalQuantity: 2,
+            checkoutUrl: 'https://example.com/cart/checkout',
+            lines: [
+                {
+                    merchandise: {
+                        product: {
+                            id: 'product-1',
+                            title: 'Product 1',
+                            vendor: 'Vendor 1'
+                        },
+                        id: 'variant-1',
+                        title: 'Variant 1',
+                        price: {
+                            amount: '10.00',
+                            currencyCode: 'USD'
+                        }
+                    },
+                    quantity: 1
+                },
+                {
+                    merchandise: {
+                        product: {
+                            id: 'product-2',
+                            title: 'Product 2',
+                            vendor: 'Vendor 2'
+                        },
+                        id: 'variant-2',
+                        title: 'Variant 2',
+                        price: {
+                            amount: '20.00',
+                            currencyCode: 'USD'
+                        }
+                    },
+                    quantity: 1
+                }
+            ],
+            cost: {
+                totalAmount: {
+                    amount: '30.00',
+                    currencyCode: 'USD'
+                }
+            }
+        } as any;
+
+        const locale: Locale = {
+            locale: 'en-US',
+            language: 'EN',
+            country: 'US'
+        };
+
+        beforeEach(() => {
+            (window as any).dataLayer = [];
+            (window as any).uetq = [];
+            window.location.href = '';
+        });
+
+        it(`should throw an error when cart is empty`, async () => {
+            const emptyCart: CartWithActions = {
+                totalQuantity: 0,
+                checkoutUrl: 'https://example.com/cart/checkout',
+                lines: [],
+                cost: {
+                    totalAmount: {
+                        amount: '0.00',
+                        currencyCode: 'USD'
+                    }
+                }
+            } as any;
+
+            await expect(Checkout({ cart: emptyCart, locale })).rejects.toThrow('Cart is empty!');
+        });
+
+        it(`should throw an error when cart is missing checkoutUrl`, async () => {
+            const cartWithoutCheckoutUrl: CartWithActions = {
+                totalQuantity: 2,
+                checkoutUrl: '',
+                lines: [
+                    {
+                        merchandise: {
+                            product: {
+                                id: 'product-1',
+                                title: 'Product 1',
+                                vendor: 'Vendor 1'
+                            },
+                            id: 'variant-1',
+                            title: 'Variant 1',
+                            price: {
+                                amount: '10.00',
+                                currencyCode: 'USD'
+                            }
+                        },
+                        quantity: 1
+                    }
+                ],
+                cost: {
+                    totalAmount: {
+                        amount: '10.00',
+                        currencyCode: 'USD'
+                    }
+                }
+            } as any;
+
+            await expect(Checkout({ cart: cartWithoutCheckoutUrl, locale })).rejects.toThrow(
+                'Cart is missing checkoutUrl'
+            );
+        });
+
+        it(`should track the begin_checkout event in Google Analytics`, async () => {
+            await Checkout({ cart, locale });
+
+            expect((window as any).dataLayer).toContainEqual({
+                event: 'begin_checkout',
+                ecommerce: {
+                    currency: 'USD',
+                    value: 30,
+                    items: [
+                        {
+                            item_id: 'product-id',
+                            item_name: 'Product 1',
+                            item_variant: 'Variant 1',
+                            item_brand: 'Vendor 1',
+                            currency: 'USD',
+                            price: 10,
+                            quantity: 1
+                        },
+                        {
+                            item_id: 'product-id',
+                            item_name: 'Product 2',
+                            item_variant: 'Variant 2',
+                            item_brand: 'Vendor 2',
+                            currency: 'USD',
+                            price: 20,
+                            quantity: 1
+                        }
+                    ]
+                }
+            });
+        });
+    });
+});
