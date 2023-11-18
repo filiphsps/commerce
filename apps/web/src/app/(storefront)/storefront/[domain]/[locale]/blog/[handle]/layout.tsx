0827d65fe854b2f411dd99865587665a6d27487d
Filiph Siitam Sandström <filiph@nordcom.io>
2023-11-18 04:41:23 +0100

    ArticlePage: Add layout stub.

---
+++ apps/web/src/app/(storefront)/storefront/[domain]/[locale]/blog/[handle]/layout.tsx
@@ -0,0 +1,7 @@
+import type { ReactNode } from 'react';
+
+/* c8 ignore start */
+export default async function ArticleLayout({ children }: { children: ReactNode }) {
+    return <>{children}</>;
+}
+/* c8 ignore stop */
