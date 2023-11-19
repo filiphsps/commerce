8935eb92e394f9b1e613eadcddd7af85ae6118bc
Filiph Siitam Sandström <filiph@nordcom.io>
2023-11-19 02:27:12 +0100

    HOTFIX.

--- apps/web/src/utils/abstract-api.ts
+++ apps/web/src/utils/abstract-api.ts
@@ -35,7 +35,7 @@ export type AbstractShopifyApolloApiBuilder<Q> = AbstractApiBuilder<ApolloClient
 export const ShopifyApolloApiBuilder: AbstractShopifyApolloApiBuilder<DocumentNode | TypedDocumentNode<any, any>> = ({
     api,
     locale,
-    fetchPolicy = 'force-cache' as any
+    fetchPolicy = 'cache-first'
 }) => ({
     locale: () => locale,
     query: async (query, variables = {}, tags = []) => {
