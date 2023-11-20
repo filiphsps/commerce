402da22cc5db71e46199b099ba48e569651e3c3d
Filiph Siitam Sandström <filiph@nordcom.io>
2023-11-20 17:28:35 +0100

    API: Add support for `http get` webhooks to `revalidate`.

--- apps/web/src/app/api/revalidate/route.ts
+++ apps/web/src/app/api/revalidate/route.ts
@@ -1,11 +1,32 @@
 import { revalidateTag } from 'next/cache';
-import { NextResponse } from 'next/server';
+import { NextResponse, type NextRequest } from 'next/server';
 
-export async function POST() {
+/* c8 ignore start */
+const revalidate = async (req: NextRequest) {
+    // TODO: Detect if prismic or shopify request.
     revalidateTag('prismic');
 
+    const body = req.json();
+    console.warn(body);
+
+    // TODO: API response builder or similar.
     return NextResponse.json({
-        revalidated: true,
-        now: Date.now()
+        status: 200,
+        errors: null,
+        data: {
+            revalidated: true,
+        },
+        metrics: {
+            now: Date.now()
+        }
     });
+};
+
+export async function POST(req: NextRequest) {
+    return await revalidate(req);
 }
+
+export async function GET(req: NextRequest) {
+    return await revalidate(req);
+}
+/* c8 ignore stop */
