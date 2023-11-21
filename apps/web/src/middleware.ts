2aec072455239675edb8efb067f5d2a3d76b0266
Filiph Siitam Sandström <filiph@nordcom.io>
2023-11-21 02:32:48 +0100

    Middleware: Attempt to speed up using matcher.

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -4,18 +4,10 @@ import { router } from '@/middleware/router';
 
 export const runtime = 'experimental-edge';
 export const config = {
-    matcher: ['/:path*']
+    // matcher: ['/:path*']
+    matcher: ['/((?!_next|_static|slice-simulator|storefront).*)', 'favicon.png', 'dynamic-sitemap.xml']
 };
 
 export default async function middleware(req: NextRequest) {
-    if (
-        req.nextUrl.pathname.startsWith('/_next') ||
-        req.nextUrl.pathname.startsWith('/_static') ||
-        req.nextUrl.pathname.startsWith('/storefront/') ||
-        req.nextUrl.pathname.startsWith('/slice-simulator') // TODO: This should probably live in the storefront.
-    ) {
-        return NextResponse.next();
-    }
-
-    return await router(req);
+    return router(req);
 }
