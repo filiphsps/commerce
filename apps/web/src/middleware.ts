a74c68e74a0a9bef4b98cd710987a58f9d39fb3f
Filiph Siitam Sandström <filiph@nordcom.io>
2023-11-21 02:35:19 +0100

    Middleware: Rearrange.

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -5,7 +5,7 @@ import { router } from '@/middleware/router';
 export const runtime = 'experimental-edge';
 export const config = {
     // matcher: ['/:path*']
-    matcher: ['/((?!_next|_static|slice-simulator|storefront).*)', 'favicon.png', 'dynamic-sitemap.xml']
+    matcher: ['/((?!storefront|_next|_static).*)', 'favicon.png', 'dynamic-sitemap.xml']
 };
 
 export default async function middleware(req: NextRequest) {
