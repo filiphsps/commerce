c54f1fe4ded9694ad73cd03ea9e75e9e590f69d6
Filiph Siitam Sandström <filiph@nordcom.io>
2023-11-22 06:36:20 +0100

    AnalyticsProvider: Resolve FRO-21.

--- apps/web/src/components/analytics-provider.tsx
+++ apps/web/src/components/analytics-provider.tsx
@@ -4,6 +4,7 @@ import type { Shop } from '@/api/shop';
 import { useAnalytics } from '@/hooks/useAnalytics';
 import { useCartUtils } from '@/hooks/useCartUtils';
 import type { Locale } from '@/utils/locale';
+import { useReportWebVitals } from 'next/web-vitals';
 import { type ReactNode } from 'react';
 
 type AnalyticsProvider = {
@@ -17,8 +18,26 @@ export const AnalyticsProvider = ({ shop, locale, children }: AnalyticsProvider)
         shop,
         pagePropsAnalyticsData: {}
     });
+
     useCartUtils({
         locale
+    });
+
+    useReportWebVitals(({ id, name, value }) => {
+        (window as any)?.dataLayer?.push({
+            event: 'web-vital',
+            event_category: 'Web Vitals',
+            event_action: name,
+            // Google Analytics metrics must be integers, so the value is rounded.
+            // For CLS the value is first multiplied by 1000 for greater precision
+            // (note: increase the multiplier for greater precision if needed).
+            event_value: Math.round(name === 'CLS' ? value * 1000 : value),
+            // The 'id' value will be unique to the current page load. When sending
+            // multiple values from the same page (e.g. for CLS), Google Analytics can
+            // compute a total by grouping on this ID (note: requires `eventLabel` to
+            // be a dimension in your report).
+            event_label: id
+        });
     });
 
     return <>{children}</>;
