eed7052cfdc3fc5b645644c228c4b9b13b0b9257
Filiph Siitam Sandström <filiph@nordcom.io>
2023-11-25 08:48:04 +0100

    ProductSearchFilters: Remove unused import.

--- apps/web/src/components/ProductSearchFilters.tsx
+++ apps/web/src/components/ProductSearchFilters.tsx
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 import { Input } from '@/components/Input';
 import { Label } from '@/components/typography/label';
