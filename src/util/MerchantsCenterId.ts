4b247f103bb0f27173429bfa16d8c49e25c4d595
Filiph Siitam Sandström <filiph@nordcom.io>
2023-12-05 10:23:10 +0100

    MerchantsCenterId: Make locale optional.

--- src/util/MerchantsCenterId.ts
+++ src/util/MerchantsCenterId.ts
@@ -6,13 +6,13 @@ export const ProductToMerchantsCenterId = ({
     productId,
     variantId
 }: {
-    locale: string;
+    locale?: string;
     productId: string;
     variantId: string;
 }) => {
     const product = parseGid(productId).resourceId;
     const variant = parseGid(variantId).resourceId;
-    const country = (locale?.split('-')[1] || 'US').toUpperCase();
+    const country = (locale?.split?.('-')?.[1] || 'US').toUpperCase();
 
     return `shopify_${country}_${product}_${variant}`;
 };
