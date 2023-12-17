1daba0b5ebdc9b990d30cbb53cb96b1b5756cf02
Filiph Siitam Sandström <filiph@nordcom.io>
2023-12-17 14:50:08 +0100

    AnalyticsProvider: Defer gtm.

--- apps/web/src/components/analytics-provider.tsx
+++ apps/web/src/components/analytics-provider.tsx
@@ -2,7 +2,7 @@
 
 import type { Shop } from '@/api/shop';
 import { Trackable } from '@/utils/trackable';
-import type { ReactNode } from 'react';
+import { useEffect, useState, type ReactNode } from 'react';
 //import { useReportWebVitals } from 'next/web-vitals';
 import { GoogleTagManager } from '@next/third-parties/google';
 import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
@@ -13,15 +13,27 @@ export type AnalyticsProviderProps = {
     children: ReactNode;
 };
 export const AnalyticsProvider = ({ shop, children }: AnalyticsProviderProps) => {
+    const [delayed, setDelayed] = useState<any>(null);
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setDelayed(
+                <>
+                    {shop?.configuration?.thirdParty?.googleTagManager ? (
+                        <GoogleTagManager gtmId={shop!.configuration!.thirdParty!.googleTagManager!} />
+                    ) : null}
+                    <VercelAnalytics debug={false} />
+                </>
+            );
+        }, 6750);
+
+        () => clearTimeout(timeout);
+    }, []);
+
     return (
         <>
-            {shop?.configuration?.thirdParty?.googleTagManager ? (
-                <GoogleTagManager gtmId={shop!.configuration!.thirdParty!.googleTagManager!} />
-            ) : null}
-            <VercelAnalytics debug={false} />
             <SpeedInsights debug={false} />
-
             <Trackable>{children}</Trackable>
+            {delayed}
         </>
     );
 };
