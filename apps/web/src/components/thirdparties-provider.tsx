0bb511627c8b9b200787ef10d61fe27221e13255
Filiph Siitam Sandström <filiph@nordcom.io>
2023-12-17 10:09:16 +0100

    Thirdparties: Simplify livechat.

--- apps/web/src/components/thirdparties-provider.tsx
+++ apps/web/src/components/thirdparties-provider.tsx
@@ -1,40 +1,15 @@
 import type { Shop } from '@/api/shop';
 import { BuildConfig } from '@/utils/build-config';
 import type { Locale } from '@/utils/locale';
-import { getClientBrowserParameters } from '@shopify/hydrogen-react';
-import { useEffect, type ReactNode } from 'react';
+import type { ReactNode } from 'react';
 import { IntercomProvider, useIntercom } from 'react-use-intercom';
 
-export type ThirdPartiesProviderProps = {
+export type LiveChatProps = {
     shop: Shop;
     locale: Locale;
     children: ReactNode;
 };
-
-export const LiveChat = ({ shop, locale, children }: ThirdPartiesProviderProps) => {
-    // TODO: Support other live chat providers.
-    if (!shop.configuration.thirdParty?.intercom || BuildConfig.environment === 'development') {
-        return <>{children}</>;
-    }
-
-    const { update } = useIntercom();
-
-    // Update attributes.
-    useEffect(() => {
-        const { uniqueToken: userId } = getClientBrowserParameters();
-
-        update({
-            userId,
-            customAttributes: {
-                locale: locale.code
-            }
-        });
-    }, [, locale]);
-
-    return <>{children}</>;
-};
-
-export const LiveChatWrapper = ({ shop, locale, children }: ThirdPartiesProviderProps) => {
+export const LiveChat = ({ shop, locale, children }: LiveChatProps) => {
     // TODO: Support other live chat providers.
     if (!shop.configuration.thirdParty?.intercom) {
         return <>{children}</>;
@@ -59,17 +34,22 @@ export const LiveChatWrapper = ({ shop, locale, children }: ThirdPartiesProvider
             }}
             initializeDelay={6750}
         >
-            <LiveChat shop={shop} locale={locale}>
-                {children}
-            </LiveChat>
+            {children}
         </IntercomProvider>
     );
 };
 
+export type ThirdPartiesProviderProps = {
+    shop: Shop;
+    locale: Locale;
+    children: ReactNode;
+};
 export const ThirdPartiesProvider = ({ shop, locale, children }: ThirdPartiesProviderProps) => {
+    if (BuildConfig.environment === 'development') return <>{children}</>;
+
     return (
-        <LiveChatWrapper shop={shop} locale={locale}>
+        <LiveChat shop={shop} locale={locale}>
             {children}
-        </LiveChatWrapper>
+        </LiveChat>
     );
 };
