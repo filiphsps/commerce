8aeecfa8148b6fcd504331540585b391fa3d7c22
Filiph Siitam Sandström <filiph@nordcom.io>
2023-12-17 09:59:19 +0100

    ProductActionsContainer: Only update quantity when it changes.

--- apps/web/src/components/products/product-actions-container.tsx
+++ apps/web/src/components/products/product-actions-container.tsx
@@ -6,6 +6,7 @@ import { InfoLines } from '@/components/products/info-lines';
 import styles from '@/components/products/product-actions-container.module.scss';
 import { ProductOptions } from '@/components/products/product-options';
 import { QuantitySelector } from '@/components/products/quantity-selector';
+import { useShop } from '@/components/shop/provider';
 import { Label } from '@/components/typography/label';
 import type { LocaleDictionary } from '@/utils/locale';
 import { Locale, useTranslation } from '@/utils/locale';
@@ -28,9 +29,9 @@ export const ProductActionsContainer = ({
     children,
     ...props
 }: ProductActionsContainerProps) => {
+    const { locale } = useShop();
     const { t } = useTranslation('common', i18n);
     const [quantity, setQuantity] = useState(1);
-    const locale = Locale.current;
 
     return (
         <ProductProvider data={product as any} initialVariantId={selectedVariant?.id || initialVariant.id}>
@@ -38,7 +39,10 @@ export const ProductActionsContainer = ({
                 <Label style={{ gridArea: 'quantity-label' }}>{t('quantity')}</Label>
 
                 <QuantitySelector
-                    update={(n) => setQuantity(n)}
+                    update={(value) => {
+                        if (value === quantity) return;
+                        setQuantity(value);
+                    }}
                     value={quantity}
                     i18n={i18n}
                     style={{ gridArea: 'quantity' }}
