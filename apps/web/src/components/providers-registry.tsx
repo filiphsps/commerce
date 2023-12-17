90ce9acc1bff2b6ad4f64f872e5561dd25d5625f
Filiph Siitam Sandström <filiph@nordcom.io>
2023-12-17 08:57:14 +0100

    Providers: Remove more traces of `ErrorBoundary`.

--- apps/web/src/components/providers-registry.tsx
+++ apps/web/src/components/providers-registry.tsx
@@ -12,7 +12,6 @@ import type { StoreModel } from '@/models/StoreModel';
 import { BuildConfig } from '@/utils/build-config';
 import { UnknownCommerceProviderError } from '@/utils/errors';
 import { Locale } from '@/utils/locale';
-import { ErrorBoundary } from '@highlight-run/next/client';
 import { PrismicProvider } from '@prismicio/react';
 import { useEffect, type ReactNode } from 'react';
 import { Toaster } from 'sonner';
@@ -55,51 +54,46 @@ export default function ProvidersRegistry({
     }
 
     return (
-        <ErrorBoundary
-            // TODO: Show the dialog.
-            showDialog={false}
-        >
-            <StyledComponentsProvider>
-                <PrismicProvider client={createClient({ shop, locale })} linkResolver={linkResolver}>
-                    <ShopifyProvider
-                        storefrontId={id}
-                        storeDomain={`https://${domain}`}
-                        storefrontApiVersion={BuildConfig.shopify.api}
-                        storefrontToken={token}
-                        countryIsoCode={(locale.country || Locale.default.country)!}
-                        languageIsoCode={locale.language}
+        <StyledComponentsProvider>
+            <PrismicProvider client={createClient({ shop, locale })} linkResolver={linkResolver}>
+                <ShopifyProvider
+                    storefrontId={id}
+                    storeDomain={`https://${domain}`}
+                    storefrontApiVersion={BuildConfig.shopify.api}
+                    storefrontToken={token}
+                    countryIsoCode={(locale.country || Locale.default.country)!}
+                    languageIsoCode={locale.language}
+                >
+                    <CartProvider
+                        cartFragment={CartFragment}
+                        languageCode={locale.language}
+                        countryCode={locale.country}
                     >
-                        <CartProvider
-                            cartFragment={CartFragment}
-                            languageCode={locale.language}
-                            countryCode={locale.country}
-                        >
-                            <ShopProvider shop={shop} currency={'USD'} locale={locale}>
-                                <HeaderProvider store={store}>
-                                    <AnalyticsProvider shop={shop}>
-                                        <ThirdPartiesProvider shop={shop} locale={locale}>
-                                            <Toaster
-                                                theme="dark"
-                                                position="bottom-left"
-                                                closeButton={true}
-                                                expand={true}
-                                                duration={5000}
-                                                gap={4}
-                                                toastOptions={{
-                                                    classNames: {
-                                                        toast: 'toast-notification'
-                                                    }
-                                                }}
-                                            />
-                                            {children}
-                                        </ThirdPartiesProvider>
-                                    </AnalyticsProvider>
-                                </HeaderProvider>
-                            </ShopProvider>
-                        </CartProvider>
-                    </ShopifyProvider>
-                </PrismicProvider>
-            </StyledComponentsProvider>
-        </ErrorBoundary>
+                        <ShopProvider shop={shop} currency={'USD'} locale={locale}>
+                            <HeaderProvider store={store}>
+                                <AnalyticsProvider shop={shop}>
+                                    <ThirdPartiesProvider shop={shop} locale={locale}>
+                                        <Toaster
+                                            theme="dark"
+                                            position="bottom-left"
+                                            closeButton={true}
+                                            expand={true}
+                                            duration={5000}
+                                            gap={4}
+                                            toastOptions={{
+                                                classNames: {
+                                                    toast: 'toast-notification'
+                                                }
+                                            }}
+                                        />
+                                        {children}
+                                    </ThirdPartiesProvider>
+                                </AnalyticsProvider>
+                            </HeaderProvider>
+                        </ShopProvider>
+                    </CartProvider>
+                </ShopifyProvider>
+            </PrismicProvider>
+        </StyledComponentsProvider>
     );
 }
