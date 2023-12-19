8c18ff6a24fcf79a26ba0e2e4b367e96b6723cf4
Filiph Siitam Sandström <filiph@nordcom.io>
2023-12-19 19:23:24 +0100

    PageProvider: Remove API call.

--- apps/web/src/components/layout/page-provider.tsx
+++ apps/web/src/components/layout/page-provider.tsx
@@ -1,14 +1,10 @@
 import 'server-only';
 
-import { HeaderApi } from '@/api/header';
 import type { Shop } from '@/api/shop';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import { Page } from '@/components/layout/page';
-import styles from '@/components/layout/page-provider.module.scss';
 import PageContent from '@/components/page-content';
-import { Content } from '@/components/typography/content';
-import { PrismicText } from '@/components/typography/prismic-text';
 import type { StoreModel } from '@/models/StoreModel';
 import type { Locale, LocaleDictionary } from '@/utils/locale';
 import { Suspense, type HTMLProps, type ReactNode } from 'react';
@@ -21,44 +17,11 @@ export type PageProviderProps = {
     children: ReactNode;
 } & Omit<HTMLProps<HTMLDivElement>, 'data' | 'className'>;
 const PageProvider = async ({ shop, store, locale, i18n, children }: PageProviderProps) => {
-    const header = await HeaderApi({ shop, locale });
-
-    const above = header?.announcements?.filter((item) => item.location === 'above') || [];
-    const bellow = header?.announcements?.filter((item) => item.location === 'bellow') || [];
-
     return (
         <>
-            {above.length > 0 && (
-                <section className={styles.announcements}>
-                    {above.map((item, index) => (
-                        <Content
-                            key={index}
-                            className={`${styles.announcement} ${
-                                item.background_color === 'primary' ? styles.primary : styles.secondary
-                            }`}
-                        >
-                            <PrismicText data={item.content} />
-                        </Content>
-                    ))}
-                </section>
-            )}
             <Suspense fallback={<Header.skeleton />}>
                 <Header shop={shop} store={store} locale={locale} i18n={i18n} />
             </Suspense>
-            {bellow.length > 0 && (
-                <section className={styles.announcements}>
-                    {bellow.map((item, index) => (
-                        <Content
-                            key={index}
-                            className={`${styles.announcement} ${
-                                item.background_color === 'primary' ? styles.primary : styles.secondary
-                            }`}
-                        >
-                            <PrismicText data={item.content} />
-                        </Content>
-                    ))}
-                </section>
-            )}
 
             {children}
 
@@ -75,7 +38,6 @@ PageProvider.skeleton = () => (
         <Page>
             <PageContent></PageContent>
         </Page>
-
         <Footer.skeleton />
     </>
 );
