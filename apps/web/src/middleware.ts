2a00cf761e4956946968f6f3a3f8a264e0fe3899
Filiph Siitam Sandström <filiph@nordcom.io>
2023-12-19 19:48:58 +0100

    Middleware: Ignore prefetch.

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -3,7 +3,11 @@ import { type NextRequest } from 'next/server';
 
 export const runtime = 'experimental-edge';
 export const config = {
-    matcher: ['/((?!storefront|admin|unknown|_next|_static|_vercel|instrumentation).*)']
+    matcher: ['/((?!storefront|admin|unknown|_next|_static|_vercel|instrumentation).*)'],
+    missing: [
+        { type: 'header', key: 'next-router-prefetch' },
+        { type: 'header', key: 'purpose', value: 'prefetch' },
+    ],
 };
 
 export default async function middleware(req: NextRequest) {
