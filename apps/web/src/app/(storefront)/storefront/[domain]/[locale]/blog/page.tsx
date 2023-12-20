006ff40102e4e10c844cbc86eca4ac1fcb79d38d
Filiph Siitam Sandström <filiph@nordcom.io>
2023-12-20 05:01:46 +0100

    BlogPage: Modernize.

--- apps/web/src/app/(storefront)/storefront/[domain]/[locale]/blog/page.tsx
+++ apps/web/src/app/(storefront)/storefront/[domain]/[locale]/blog/page.tsx
@@ -1,8 +1,8 @@
 import { PageApi } from '@/api/page';
-import { ShopApi, ShopsApi } from '@/api/shop';
+import { ShopApi } from '@/api/shop';
 import { ShopifyApiClient, ShopifyApolloApiClient } from '@/api/shopify';
 import { BlogApi } from '@/api/shopify/blog';
-import { LocalesApi, StoreApi } from '@/api/store';
+import { LocalesApi } from '@/api/store';
 import PrismicPage from '@/components/prismic-page';
 import Heading from '@/components/typography/heading';
 import { getDictionary } from '@/i18n/dictionary';
@@ -12,41 +12,11 @@ import { Prefetch } from '@/utils/prefetch';
 import { asText } from '@prismicio/client';
 import type { Metadata } from 'next';
 import { notFound } from 'next/navigation';
-import { metadata as notFoundMetadata } from '../not-found';
 import BlogContent from './blog-content';
 
-/* c8 ignore start */
-export const revalidate = 28_800; // 8hrs.
 export const dynamicParams = true;
-export async function generateStaticParams() {
-    const locale = Locale.default;
-    const shops = await ShopsApi();
 
-    return (
-        await Promise.all(
-            shops
-                .map(async (shop) => {
-                    try {
-                        const api = await ShopifyApiClient({ shop, locale });
-                        const locales = await LocalesApi({ api });
-
-                        return locales.map(({ code }) => ({
-                            domain: shop.domains.primary,
-                            locale: code
-                        }));
-                    } catch {
-                        return null;
-                    }
-                })
-                .filter((_) => _)
-        )
-    ).flat(2);
-}
-/* c8 ignore stop */
-
-/* c8 ignore start */
 export type BlogPageParams = { domain: string; locale: string };
-
 export async function generateMetadata({
     params: { domain, locale: localeData }
 }: {
@@ -55,10 +25,9 @@ export async function generateMetadata({
     try {
         const shop = await ShopApi({ domain });
         const locale = Locale.from(localeData);
-        if (!locale) return notFoundMetadata;
+        if (!locale) return notFound();
 
         const api = await ShopifyApolloApiClient({ shop, locale });
-        const store = await StoreApi({ api });
         const { page } = await PageApi({ shop, locale, handle: 'blog', type: 'custom_page' });
         const locales = await LocalesApi({ api });
 
@@ -83,7 +52,7 @@ export async function generateMetadata({
                 type: 'website',
                 title,
                 description,
-                siteName: store?.name,
+                siteName: shop.name,
                 locale: locale.code,
                 images:
                     (page?.meta_image && [
@@ -100,7 +69,7 @@ export async function generateMetadata({
         };
     } catch (error: unknown) {
         if (Error.isNotFound(error)) {
-            return notFoundMetadata;
+            return notFound();
         }
 
         throw error;
@@ -113,12 +82,12 @@ export default async function BlogPage({ params: { domain, locale: localeData } 
         const locale = Locale.from(localeData);
         if (!locale) return notFound();
 
-        const i18n = await getDictionary(locale);
         const api = await ShopifyApolloApiClient({ shop, locale });
-        const store = await StoreApi({ api });
-        const { page } = await PageApi({ shop, locale, handle: 'blog', type: 'custom_page' });
-        const prefetch = await Prefetch({ api, page });
+        const { page } = await PageApi({ shop, locale, handle: 'blog', type: 'custom_page' })
         const blog = await BlogApi({ api, handle: 'news' });
+
+        void Prefetch({ api, page });
+        const i18n = await getDictionary(locale);
 
         return (
             <>
@@ -128,10 +97,8 @@ export default async function BlogPage({ params: { domain, locale: localeData } 
                 {page?.slices && page?.slices.length > 0 && (
                     <PrismicPage
                         shop={shop}
-                        store={store}
                         locale={locale}
                         page={page}
-                        prefetch={prefetch}
                         i18n={i18n}
                         handle={'blog'}
                         type={'custom_page'}
