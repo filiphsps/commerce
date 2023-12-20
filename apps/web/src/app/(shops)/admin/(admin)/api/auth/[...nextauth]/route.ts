6497e74ea8fe3fe77ec9fb14c8ddb9237c9e2cf8
Filiph Siitam Sandström <filiph@nordcom.io>
2023-12-20 01:42:44 +0100

    Admin: Switch auth API to `nodejs`.

--- apps/web/src/app/(shops)/admin/(admin)/api/auth/[...nextauth]/route.ts
+++ apps/web/src/app/(shops)/admin/(admin)/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,8 @@
 import { authOptions } from '#/utils/auth';
-import { withEdgeHighlight } from '@/utils/config/highlight.edge';
+import { withAppRouterHighlight } from '@/utils/config/highlight.app';
 import NextAuth from 'next-auth';
 
-export const runtime = 'experimental-edge';
-const handler = withEdgeHighlight(NextAuth(authOptions) as any);
+export const runtime = 'nodejs';
+const handler = withAppRouterHighlight(NextAuth(authOptions) as any);
 
 export { handler as GET, handler as POST };
