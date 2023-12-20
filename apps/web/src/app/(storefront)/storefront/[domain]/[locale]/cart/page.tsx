aca5de51a01a332ae56c479aa4932b3bbc519efd
Filiph Siitam Sandström <filiph@nordcom.io>
2023-12-20 05:08:00 +0100

    Cart: Remove legacy model.

--- apps/web/src/app/(storefront)/storefront/[domain]/[locale]/cart/page.tsx
+++ apps/web/src/app/(storefront)/storefront/[domain]/[locale]/cart/page.tsx
@@ -1,7 +1,6 @@
 import { PageApi } from '@/api/page';
-import { ShopApi, ShopsApi } from '@/api/shop';
+import { ShopApi } from '@/api/shop';
 import { ShopifyApiClient, ShopifyApolloApiClient } from '@/api/shopify';
-import { LocalesApi, StoreApi } from '@/api/store';
 import { Page } from '@/components/layout/page';
 import PageContent from '@/components/page-content';
 import PrismicPage from '@/components/prismic-page';
@@ -13,39 +12,10 @@ import { Prefetch } from '@/utils/prefetch';
 import { asText } from '@prismicio/client';
 import type { Metadata } from 'next';
 import { notFound } from 'next/navigation';
-import { metadata as notFoundMetadata } from '../not-found';
 import CartContent from './cart-content';
 
-/* c8 ignore start */
-export const revalidate = 28_800; // 8hrs.
 export const dynamicParams = true;
-export async function generateStaticParams() {
-    const locale = Locale.default;
-    const shops = await ShopsApi();
 
-    return (
-        await Promise.all(
-            shops
-                .map(async (shop) => {
-                    try {
-                        const api = await ShopifyApiClient({ shop, locale });
-                        const locales = await LocalesApi({ api });
-
-                        return locales.map(({ code }) => ({
-                            domain: shop.domains.primary,
-                            locale: code
-                        }));
-                    } catch {
-                        return null;
-                    }
-                })
-                .filter((_) => _)
-        )
-    ).flat(2);
-}
-/* c8 ignore stop */
-
-/* c8 ignore start */
 export type CartPageParams = { domain: string; locale: string };
 export async function generateMetadata({
     params: { domain, locale: localeData }
@@ -53,14 +23,13 @@ export async function generateMetadata({
     params: CartPageParams;
 }): Promise<Metadata> {
     try {
-        const shop = await ShopApi({ domain });
         const locale = Locale.from(localeData);
-        if (!locale) return notFoundMetadata;
+        if (!locale) return notFound();
 
+        const shop = await ShopApi({ domain, locale });
         const api = await ShopifyApolloApiClient({ shop, locale });
-        const store = await StoreApi({ api });
-        const locales = await LocalesApi({ api });
         const { page } = await PageApi({ shop, locale, handle: 'cart', type: 'custom_page' });
+        
         const i18n = await getDictionary(locale);
         const { t } = useTranslation('common', i18n);
 
@@ -71,21 +40,14 @@ export async function generateMetadata({
             title,
             description,
             alternates: {
-                canonical: `https://${shop.domains.primary}/${locale.code}/cart/`,
-                languages: locales.reduce(
-                    (prev, { code }) => ({
-                        ...prev,
-                        [code]: `https://${shop.domains.primary}/${code}/cart/`
-                    }),
-                    {}
-                )
+                canonical: `https://${shop.domains.primary}/${locale.code}/cart/`
             },
             openGraph: {
                 url: `/cart/`,
                 type: 'website',
                 title,
                 description,
-                siteName: store?.name,
+                siteName: shop?.name,
                 locale: locale.code,
                 images:
                     (page?.meta_image && [
@@ -102,7 +64,7 @@ export async function generateMetadata({
         };
     } catch (error: unknown) {
         if (Error.isNotFound(error)) {
-            return notFoundMetadata;
+            return notFound();
         }
 
         throw error;
@@ -112,15 +74,15 @@ export async function generateMetadata({
 
 export default async function CartPage({ params: { domain, locale: localeData } }: { params: CartPageParams }) {
     try {
-        const shop = await ShopApi({ domain });
         const locale = Locale.from(localeData);
         if (!locale) return notFound();
-        const i18n = await getDictionary(locale);
 
+        const shop = await ShopApi({ domain, locale });
         const api = await ShopifyApolloApiClient({ shop, locale });
-        const store = await StoreApi({ api });
         const { page } = await PageApi({ shop, locale, handle: 'cart', type: 'custom_page' });
-        const prefetch = await Prefetch({ api, page });
+        
+        void Prefetch({ api, page });
+        const i18n = await getDictionary(locale);
 
         return (
             <Page>
@@ -137,10 +99,8 @@ export default async function CartPage({ params: { domain, locale: localeData } 
                             page ? (
                                 <PrismicPage
                                     shop={shop}
-                                    store={store}
                                     locale={locale}
                                     page={page}
-                                    prefetch={prefetch}
                                     i18n={i18n}
                                     handle={'cart'}
                                     type={'custom_page'}
