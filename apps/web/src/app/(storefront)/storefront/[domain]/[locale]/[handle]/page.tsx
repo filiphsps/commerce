c679563b33d3b302c5a18caeba35f6e16a92933c
Filiph Siitam Sandström <filiph@nordcom.io>
2023-12-20 04:46:14 +0100

    CustomPage: Remove pre rendering.

--- apps/web/src/app/(storefront)/storefront/[domain]/[locale]/[handle]/page.tsx
+++ apps/web/src/app/(storefront)/storefront/[domain]/[locale]/[handle]/page.tsx
@@ -1,9 +1,9 @@
 import 'server-only';
 
-import { PageApi, PagesApi } from '@/api/page';
-import { ShopApi, ShopsApi } from '@/api/shop';
+import { PageApi } from '@/api/page';
+import { ShopApi } from '@/api/shop';
 import { ShopifyApiClient, ShopifyApolloApiClient } from '@/api/shopify';
-import { LocalesApi, StoreApi } from '@/api/store';
+import { LocalesApi } from '@/api/store';
 import { Page } from '@/components/layout/page';
 import PageContent from '@/components/page-content';
 import PrismicPage from '@/components/prismic-page';
@@ -14,31 +14,9 @@ import { Locale } from '@/utils/locale';
 import { asText } from '@prismicio/client';
 import type { Metadata } from 'next';
 import { notFound } from 'next/navigation';
-import { metadata as notFoundMetadata } from '../not-found';
 import { Suspense } from 'react';
 
 export const dynamicParams = true;
-export async function generateStaticParams() {
-    const shops = await ShopsApi();
-
-    return await Promise.all(
-        shops.map(async (shop) => {
-            const api = await ShopifyApiClient({ shop, locale: Locale.default });
-            const locales = await LocalesApi({ api });
-
-            return await Promise.all(
-                locales.map(async (locale) => {
-                    const pages = await PagesApi({ shop, locale });
-                    return pages.map(({ uid: handle }) => ({
-                        domain: shop.domains.primary,
-                        locale: locale.code,
-                        handle
-                    }));
-                })
-            );
-        })
-    );
-}
 
 export type CustomPageParams = { domain: string; locale: string; handle: string };
 export async function generateMetadata({
@@ -47,32 +25,26 @@ export async function generateMetadata({
     params: CustomPageParams;
 }): Promise<Metadata> {
     try {
-        if (!isValidHandle(handle)) return notFoundMetadata;
+        if (!isValidHandle(handle)) return notFound();
 
         const locale = Locale.from(localeData);
-        if (!locale) return notFoundMetadata;
+        if (!locale) return notFound();
 
         const shop = await ShopApi({ domain, locale });
-
-        // Next.js Preloading pattern.
-        PageApi.preload({ shop, locale, handle });
-
         // Setup the AbstractApi client.
         const api = await ShopifyApolloApiClient({ shop, locale });
-
         // Do the actual API calls.
-        const store = await StoreApi({ api });
-
         const { page } = await PageApi({ shop, locale, handle });
-        if (!page) return notFoundMetadata;
+        if (!page) return notFound();
+        // Extra calls,
+        const locales = await LocalesApi({ api });
 
-        const locales = store.i18n?.locales || [Locale.default];
         // If the page is the homepage we shouldn't add the handle to path.
         // TODO: Deal with this in a better way.
         const path = handle === 'homepage' ? '/' : `/${handle}/`;
+
         const title = page.meta_title || page.title || handle;
         const description = (page.meta_description && asText(page.meta_description)) || page.description || undefined;
-
         return {
             title,
             description,
@@ -90,7 +62,7 @@ export async function generateMetadata({
         };
     } catch (error: unknown) {
         if (Error.isNotFound(error)) {
-            return notFoundMetadata;
+            return notFound();
         }
 
         throw error;
@@ -111,15 +83,8 @@ export default async function CustomPage({
 
         // Fetch the current shop.
         const shop = await ShopApi({ domain, locale });
-
-        // Next.js Preloading pattern.
-        PageApi.preload({ shop, locale, handle });
-
         // Setup the AbstractApi client.
         const api = await ShopifyApolloApiClient({ shop, locale });
-
-        // Do the actual API calls.
-        const store = await StoreApi({ api });
 
         const { page } = await PageApi({ shop, locale, handle });
         if (!page) return notFound(); // TODO: Return proper error.
@@ -133,7 +98,6 @@ export default async function CustomPage({
                     <Suspense key={`${shop.id}.page.${handle}`} fallback={<PrismicPage.skeleton page={page} />}>
                         <PrismicPage
                             shop={shop}
-                            store={store}
                             locale={locale}
                             page={page}
                             i18n={i18n}
