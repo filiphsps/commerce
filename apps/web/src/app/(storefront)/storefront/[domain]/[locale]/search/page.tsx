3d087be39a981ba4597488dae7c964e4b6402edb
Filiph Siitam Sandström <filiph@nordcom.io>
2023-12-20 05:21:25 +0100

    SearchPage: Remove legacy props.

--- apps/web/src/app/(storefront)/storefront/[domain]/[locale]/search/page.tsx
+++ apps/web/src/app/(storefront)/storefront/[domain]/[locale]/search/page.tsx
@@ -1,7 +1,6 @@
 import { PageApi } from '@/api/page';
-import { ShopApi, ShopsApi } from '@/api/shop';
+import { ShopApi } from '@/api/shop';
 import { ShopifyApiClient, ShopifyApolloApiClient } from '@/api/shopify';
-import { LocalesApi, StoreApi } from '@/api/store';
 import { Page } from '@/components/layout/page';
 import PageContent from '@/components/page-content';
 import PrismicPage from '@/components/prismic-page';
@@ -14,40 +13,10 @@ import { asText } from '@prismicio/client';
 import type { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 import { Suspense } from 'react';
-import { metadata as notFoundMetadata } from '../not-found';
 import SearchContent from './search-content';
 
-/* c8 ignore start */
-export const revalidate = 28_800; // 8hrs.
 export const dynamicParams = true;
-export async function generateStaticParams() {
-    const locale = Locale.default;
-    const shops = await ShopsApi();
 
-    return (
-        await Promise.all(
-            shops
-                .map(async (shop) => {
-                    try {
-                        const api = await ShopifyApiClient({ shop, locale });
-                        const locales = await LocalesApi({ api });
-                        const domain = shop.domains.primary;
-
-                        return locales.map(({ code }) => ({
-                            domain,
-                            locale: code
-                        }));
-                    } catch {
-                        return null;
-                    }
-                })
-                .filter((_) => _)
-        )
-    ).flat(2);
-}
-/* c8 ignore stop */
-
-/* c8 ignore start */
 export type SearchPageParams = { domain: string; locale: string };
 export async function generateMetadata({
     params: { domain, locale: localeData }
@@ -55,39 +24,30 @@ export async function generateMetadata({
     params: SearchPageParams;
 }): Promise<Metadata> {
     try {
-        const shop = await ShopApi({ domain });
         const locale = Locale.from(localeData);
-        if (!locale) return notFoundMetadata;
+        if (!locale) return notFound();
+
+        const shop = await ShopApi({ domain, locale });
 
         const api = await ShopifyApolloApiClient({ shop, locale });
-        const store = await StoreApi({ api });
-        const locales = await LocalesApi({ api });
         const { page } = await PageApi({ shop, locale, handle: 'search', type: 'custom_page' });
         const i18n = await getDictionary(locale);
         const { t } = useTranslation('common', i18n);
 
         const title = page?.meta_title || page?.title || t('search');
         const description = (page?.meta_description && asText(page.meta_description)) || page?.description || undefined;
-
         return {
             title,
             description,
             alternates: {
-                canonical: `https://${shop.domains.primary}/${locale.code}/search/`,
-                languages: locales.reduce(
-                    (prev, { code }) => ({
-                        ...prev,
-                        [code]: `https://${shop.domains.primary}/${code}/search/`
-                    }),
-                    {}
-                )
+                canonical: `https://${shop.domains.primary}/${locale.code}/search/`
             },
             openGraph: {
                 url: `/search/`,
                 type: 'website',
                 title,
                 description,
-                siteName: store?.name,
+                siteName: shop.name,
                 locale: locale.code,
                 images:
                     (page?.meta_image && [
@@ -104,7 +64,7 @@ export async function generateMetadata({
         };
     } catch (error: unknown) {
         if (Error.isNotFound(error)) {
-            return notFoundMetadata;
+            return notFound();
         }
 
         throw error;
@@ -117,13 +77,13 @@ export default async function SearchPage({ params: { domain, locale: localeData 
         const shop = await ShopApi({ domain });
         const locale = Locale.from(localeData);
         if (!locale) return notFound();
-        const i18n = await getDictionary(locale);
 
         const api = await ShopifyApolloApiClient({ shop, locale });
         const store = await StoreApi({ api });
-
         const { page } = await PageApi({ shop, locale, handle: 'search', type: 'custom_page' });
-        const prefetch = await Prefetch({ api, page });
+        
+        void Prefetch({ api, page });
+        const i18n = await getDictionary(locale);
 
         return (
             <Page>
@@ -133,10 +93,8 @@ export default async function SearchPage({ params: { domain, locale: localeData 
                     {page?.slices && page?.slices.length > 0 && (
                         <PrismicPage
                             shop={shop}
-                            store={store}
                             locale={locale}
                             page={page}
-                            prefetch={prefetch}
                             i18n={i18n}
                             handle={'search'}
                             type={'custom_page'}
