3545251db9d261f7cbf9f9a83612cd38ea211e61
Filiph Siitam Sandström <filiph@nordcom.io>
2024-03-09 16:41:37 +0100

    Packages: Add package for common marketing assets/utils/etc.

---
+++ packages/marketing-common/src/index.ts
@@ -0,0 +1,12 @@
+import type { NordstarTheme } from '@nordcom/nordstar';
+
+export const Theme: NordstarTheme = {
+    accents: {
+        primary: '#ed1e79',
+        secondary: '#ed1e79'
+    },
+    fonts: {
+        heading: 'var(--font-primary)',
+        body: 'var(--font-primary)'
+    }
+};
