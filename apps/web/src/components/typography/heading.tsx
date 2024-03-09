bfcfd86b19f10f8be148118601c9b50861b09e20
Filiph Siitam Sandström <filiph@nordcom.io>
2024-03-09 08:25:09 +0100

    Storefront: Improve heading.

--- apps/web/src/components/typography/heading.tsx
+++ apps/web/src/components/typography/heading.tsx
@@ -1,29 +1,49 @@
+import type { As } from '@nordcom/nordstar';
 import type { CSSProperties, ComponentProps, ElementType, HTMLProps, ReactNode } from 'react';
 
+import { Fragment } from 'react';
+
 import styles from '@/components/typography/heading.module.scss';
-import type { As } from '@nordcom/nordstar';
 
 export type TitleProps<T extends As> = {
     as?: As;
     children?: ReactNode;
     bold?: boolean;
 } & ComponentProps<T>;
-export const Title = <T extends As = ElementType<'h1'>>({ as, bold, className, ...props }: TitleProps<T>) => {
+export const Title = <T extends As = ElementType<'h1'>>({ as, bold, className, key, ...props }: TitleProps<T>) => {
+    if (as === null) {
+        return <Fragment key={key} children={props.children} />;
+    }
+
     const AsComponent = as || ('h1' as keyof JSX.IntrinsicElements);
-    return <AsComponent {...props} className={`${styles.title} ${(bold && styles.bold) || ''} ${className || ''}`} />;
+    return (
+        <AsComponent
+            key={key}
+            {...props}
+            className={`${styles.title} ${(bold && styles.bold) || ''} ${className || ''}`}
+        />
+    );
 };
 
 export type SubTitleProps = {
     children?: ReactNode;
-    as?: ElementType;
+    as?: As | null;
     bold?: boolean;
 } & HTMLProps<HTMLDivElement>;
-export const SubTitle = ({ as, bold, className, ...props }: SubTitleProps) => {
+export const SubTitle = ({ as, bold, className, key, ...props }: SubTitleProps) => {
+    if (as === null) {
+        return <Fragment key={key} children={props.children} />;
+    }
+
     const fallback: keyof JSX.IntrinsicElements = 'div';
     const AsComponent = as || fallback;
 
     return (
-        <AsComponent {...props} className={`${styles.subtitle} ${(bold && styles.bold) || ''} ${className || ''}`} />
+        <AsComponent
+            key={key}
+            {...props}
+            className={`${styles.subtitle} ${(bold && styles.bold) || ''} ${className || ''}`}
+        />
     );
 };
 
@@ -32,11 +52,11 @@ type HeadingProps = {
     subtitle?: ReactNode;
     reverse?: boolean;
     bold?: boolean;
-    titleAs?: ElementType;
+    titleAs?: As | null;
     titleStyle?: CSSProperties;
     titleClassName?: string;
     titleProps?: ComponentProps<any>;
-    subtitleAs?: ElementType;
+    subtitleAs?: As | null;
     subtitleStyle?: CSSProperties;
     subtitleClassName?: string;
 };
@@ -58,6 +78,7 @@ const Heading = ({
             {title}
         </Title>
     );
+
     const subtitleComponent = subtitle ? (
         <SubTitle bold={bold} as={subtitleAs as any} style={subtitleStyle} className={subtitleClassName}>
             {subtitle}
