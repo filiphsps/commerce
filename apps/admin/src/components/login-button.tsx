4876b981f59c3aea19265aee9917c4294d822d8d
Filiph Siitam Sandström <filiph@nordcom.io>
2024-03-23 07:51:25 +0100

    Admin: Revert `login-button`.

--- apps/admin/src/components/login-button.tsx
+++ apps/admin/src/components/login-button.tsx
@@ -1,14 +1,17 @@
-import 'server-only';
+'use client';
 
 import styles from '@/components/login-button.module.scss';
 
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { signIn } from 'next-auth/react';
 
 import { UnknownApiError } from '@nordcom/commerce-errors';
 import { Button } from '@nordcom/nordstar';
 
 import GithubLight from '@/static/icons/light/github.svg';
-import { signIn } from '@/utils/auth';
+import { toast } from 'sonner';
 
 import type { AuthProvider } from '@/utils/auth';
 import type { HTMLProps, ReactNode } from 'react';
@@ -16,13 +19,36 @@ import type { HTMLProps, ReactNode } from 'react';
 export type LoginButtonProps = {
     provider?: AuthProvider['name'];
 } & Omit<HTMLProps<HTMLButtonElement>, 'type' | 'onClick'>;
-export default async function LoginButton({ provider = 'github', className, ...props }: LoginButtonProps) {
-    let content: string;
-    let icon: ReactNode = <></>;
+export default function LoginButton({ provider = 'github', className, ...props }: LoginButtonProps) {
+    const [loading, setLoading] = useState<boolean>(false);
 
+    // Get error message added by next/auth in URL.
+    const router = useRouter();
+    const path = usePathname();
+    const searchParams = useSearchParams();
+    const error = searchParams.get('error');
+
+    useEffect(() => {
+        const errorMessage = Array.isArray(error) ? error.pop() : error;
+        if (!errorMessage) return;
+
+        console.error(errorMessage);
+        toast.error(errorMessage);
+
+        const params = new URLSearchParams(searchParams);
+
+        // Delete error message from URL.
+        params.delete('error');
+        params.delete('callbackUrl');
+
+        router.replace(`${path}${params.size > 0 ? '?' : ''}${params.toString()}`);
+    }, [error]);
+
+    let layout: ReactNode = <></>;
+    let icon: ReactNode = <></>;
     switch (provider) {
         case 'github': {
-            content = 'Login with GitHub';
+            layout = 'Login with GitHub';
             icon = <Image className={styles['provider-logo']} src={GithubLight} alt="GitHub" />;
             break;
         }
@@ -36,19 +62,30 @@ export default async function LoginButton({ provider = 'github', className, ...p
             {...props}
             color="default"
             variant="outline"
-            onClick={async () => {
-                'use server';
+            onClick={() => {
+                if (loading) {
+                    toast.error('Please wait for the current request to finish.');
+                    return;
+                }
 
-                await signIn(provider, {
-                    redirectTo: '/'
-                });
+                switch (provider) {
+                    case 'github': {
+                        setLoading(true);
+                        signIn('github');
+                        break;
+                    }
+                    default: {
+                        throw new UnknownApiError();
+                    }
+                }
             }}
             type="button"
             as="button"
-            className={`${styles.container} ${className || ''}`}
+            disabled={loading}
+            className={`${styles.container} ${loading ? styles.loading : ''} ${className || ''}`}
             icon={icon}
         >
-            {content}
+            {!loading ? layout : <p>Loading...</p>}
         </Button>
     );
 }
