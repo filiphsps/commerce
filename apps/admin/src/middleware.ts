80b3288c4a363beb2d70afe7d660e8ceab67707e
Filiph Siitam Sandström <filiph@nordcom.io>
2024-03-23 02:49:35 +0100

    Admin: Simplify middleware.

--- apps/admin/src/middleware.ts
+++ apps/admin/src/middleware.ts
@@ -3,13 +3,7 @@ import NextAuth from 'next-auth';
 
 import authConfig from '@/utils/auth.config';
 
-const { auth } = NextAuth(authConfig);
-
-export default auth(async (req) => {
-    return NextResponse.next({
-        request: req
-    });
-});
+export const { auth: middleware } = NextAuth(authConfig)
 
 export const config = {
     matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
