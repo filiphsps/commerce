a610137151d1f2ef3e0a8b93fac0b063154cf121
Filiph Siitam Sandström <filiph@nordcom.io>
2024-08-12 13:53:42 +0200

    Storefront: Improve banner-aside button.

--- apps/storefront/src/slices/common/Banner/banner-aside.tsx
+++ apps/storefront/src/slices/common/Banner/banner-aside.tsx
@@ -43,7 +43,7 @@ export const BannerAside = ({ slice }: { slice: BannerSliceAside }): JSX.Element
                             <Link
                                 key={index}
                                 className={cn(
-                                    'flex items-center gap-2 rounded-full bg-white px-4 py-2 text-black drop-shadow-sm transition-colors hover:bg-black hover:text-white md:px-6 md:py-3 md:text-lg',
+                                    'flex items-center gap-2 rounded-full bg-white px-4 py-2 text-black drop-shadow-sm transition-colors hover:bg-black hover:text-white md:px-6 md:py-3 md:text-lg font-semibold',
                                     type &&
                                         'bg-secondary text-secondary-foreground hover:bg-secondary-dark hover:text-secondary-foreground'
                                 )}
