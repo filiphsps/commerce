b69dcafd53ba9dec8138237338445f7a26cbd583
Filiph Siitam Sandström <filiph@nordcom.io>
2024-08-22 05:11:22 +0200

    Storefront: Handle missing footer.

--- apps/storefront/src/components/Footer/Footer.tsx
+++ apps/storefront/src/components/Footer/Footer.tsx
@@ -24,76 +24,81 @@ export type FooterProps = {
 const Footer = async ({ shop, locale, i18n }: FooterProps) => {
     const api = await ShopifyApolloApiClient({ shop, locale });
 
-    const footer = await FooterApi({ shop, locale });
-    const store = await StoreApi({ api, locale });
+    try {
+        const footer = await FooterApi({ shop, locale });
+        const store = await StoreApi({ api, locale });
 
-    const borderStyles = 'border-0 border-b-4 border-solid border-primary-light pb-8 lg:border-0 lg:pb-0';
+        const borderStyles = 'border-0 border-b-4 border-solid border-primary-light pb-8 lg:border-0 lg:pb-0';
 
-    // TODO: Dynamic copyright copy and content.
-    return (
-        <footer className={cn(styles.container, 'flex h-full min-h-96 w-full')}>
-            <div className={cn(styles.content, 'flex h-full flex-col items-stretch gap-4 md:gap-8')}>
-                <div className={cn(styles.blocks, 'gap-6', borderStyles)}>
-                    <div className={cn(styles.block, borderStyles)}>
-                        {store.logos.primary?.src ? (
-                            <Image
-                                className={styles.logo}
-                                title={store.name}
-                                src={store.logos.primary.src}
-                                alt={store.logos.primary.alt || store.name}
-                                width={store.logos.primary.width || 0}
-                                height={store.logos.primary.height || 0}
-                                sizes="(max-width: 950px) 75px, 225px"
-                                priority={false}
-                                loading="lazy"
-                                decoding="async"
-                            />
-                        ) : null}
+        // TODO: Dynamic copyright copy and content.
+        return (
+            <footer className={cn(styles.container, 'flex h-full min-h-96 w-full')}>
+                <div className={cn(styles.content, 'flex h-full flex-col items-stretch gap-4 md:gap-8')}>
+                    <div className={cn(styles.blocks, 'gap-6', borderStyles)}>
+                        <div className={cn(styles.block, borderStyles)}>
+                            {store.logos.primary?.src ? (
+                                <Image
+                                    className={styles.logo}
+                                    title={store.name}
+                                    src={store.logos.primary.src}
+                                    alt={store.logos.primary.alt || store.name}
+                                    width={store.logos.primary.width || 0}
+                                    height={store.logos.primary.height || 0}
+                                    sizes="(max-width: 950px) 75px, 225px"
+                                    priority={false}
+                                    loading="lazy"
+                                    decoding="async"
+                                />
+                            ) : null}
 
-                        <address className="text-sm font-medium leading-snug">
-                            <PrismicText data={footer.address} styled={false} />
-                        </address>
+                            <address className="text-sm font-medium leading-snug">
+                                <PrismicText data={footer.address} styled={false} />
+                            </address>
+                        </div>
+
+                        {footer.body.map(({ primary: { title }, items }, index) => {
+                            return (
+                                <div
+                                    key={`${title}-${index}`}
+                                    className={cn(
+                                        styles.block,
+                                        'flex flex-col gap-2 empty:hidden md:items-end md:justify-end md:empty:flex'
+                                    )}
+                                    data-align="right"
+                                >
+                                    {title ? (
+                                        <div className={cn(styles.title, 'text-xl font-extrabold leading-tight')}>
+                                            {title}
+                                        </div>
+                                    ) : null}
+
+                                    {items.length > 0 ? (
+                                        <div className="flex flex-wrap gap-2 *:after:font-extrabold *:after:opacity-75 *:after:content-[','] last:*:after:content-['.']">
+                                            {items.map((item: any, index) => (
+                                                <Link
+                                                    key={item.handle || index}
+                                                    href={item.handle || ''}
+                                                    target={item.handle?.startsWith('http') ? '_blank' : ''}
+                                                    className="text-sm leading-none"
+                                                >
+                                                    {item.title}
+                                                </Link>
+                                            ))}
+                                        </div>
+                                    ) : null}
+                                </div>
+                            );
+                        })}
                     </div>
 
-                    {footer.body.map(({ primary: { title }, items }, index) => {
-                        return (
-                            <div
-                                key={`${title}-${index}`}
-                                className={cn(
-                                    styles.block,
-                                    'flex flex-col gap-2 empty:hidden md:items-end md:justify-end md:empty:flex'
-                                )}
-                                data-align="right"
-                            >
-                                {title ? (
-                                    <div className={cn(styles.title, 'text-xl font-extrabold leading-tight')}>
-                                        {title}
-                                    </div>
-                                ) : null}
-
-                                {items.length > 0 ? (
-                                    <div className="flex flex-wrap gap-2 *:after:font-extrabold *:after:opacity-75 *:after:content-[','] last:*:after:content-['.']">
-                                        {items.map((item: any, index) => (
-                                            <Link
-                                                key={item.handle || index}
-                                                href={item.handle || ''}
-                                                target={item.handle?.startsWith('http') ? '_blank' : ''}
-                                                className="text-sm leading-none"
-                                            >
-                                                {item.title}
-                                            </Link>
-                                        ))}
-                                    </div>
-                                ) : null}
-                            </div>
-                        );
-                    })}
+                    <FooterContent locale={locale} i18n={i18n} shop={shop} />
                 </div>
-
-                <FooterContent locale={locale} i18n={i18n} shop={shop} />
-            </div>
-        </footer>
-    );
+            </footer>
+        );
+    } catch {
+        // TODO: Handle error.
+        return null;
+    }
 };
 
 Footer.skeleton = () => (
