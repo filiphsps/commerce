8cb2f22f707cf713812c79918c3cbbeaff834556
Filiph Siitam Sandström <filiph@nordcom.io>
2024-09-16 03:02:39 +0200

    Storefront: Improve blog error handling.

--- apps/storefront/src/app/[domain]/[locale]/blog/[handle]/page.tsx
+++ apps/storefront/src/app/[domain]/[locale]/blog/[handle]/page.tsx
@@ -16,8 +16,9 @@ import { Content } from '@/components/typography/content';
 import Heading from '@/components/typography/heading';
 import { Label } from '@/components/typography/label';
 
+import type { Article } from '@shopify/hydrogen-react/storefront-api-types';
 import type { Metadata } from 'next';
-import type { Article, WithContext } from 'schema-dts';
+import type { Article as LdArticle, WithContext } from 'schema-dts';
 
 export const runtime = 'nodejs';
 export const dynamic = 'force-static';
@@ -34,39 +35,14 @@ export async function generateMetadata({
         notFound();
     }
 
+    const shop = await Shop.findByDomain(domain, { sensitiveData: true });
+    const locale = Locale.from(localeData);
+
+    const api = await ShopifyApolloApiClient({ shop, locale });
+
+    let article: Article;
     try {
-        const shop = await Shop.findByDomain(domain, { sensitiveData: true });
-        const locale = Locale.from(localeData);
-
-        const api = await ShopifyApolloApiClient({ shop, locale });
-        const article = await BlogArticleApi({ api, blogHandle: 'news', handle });
-        const locales = await LocalesApi({ api });
-
-        const title = article.seo?.title || article.title;
-        const description = article.seo?.description || '';
-        return {
-            title,
-            description,
-            alternates: {
-                canonical: `https://${shop.domain}/${locale.code}/blog/${handle}/`,
-                languages: locales.reduce(
-                    (prev, { code }) => ({
-                        ...prev,
-                        [code]: `https://${shop.domain}/${code}/blog/${handle}/`
-                    }),
-                    {}
-                )
-            },
-            openGraph: {
-                url: `/blog/${handle}/`,
-                type: 'article',
-                title,
-                description,
-                siteName: shop.name,
-                locale: locale.code,
-                images: []
-            }
-        };
+        article = await BlogArticleApi({ api, blogHandle: 'news', handle });
     } catch (error: unknown) {
         if (Error.isNotFound(error)) {
             notFound();
@@ -76,6 +52,33 @@ export async function generateMetadata({
         unstable_rethrow(error);
         throw error;
     }
+    const locales = await LocalesApi({ api });
+
+    const title = article.seo?.title || article.title;
+    const description = article.seo?.description || '';
+    return {
+        title,
+        description,
+        alternates: {
+            canonical: `https://${shop.domain}/${locale.code}/blog/${handle}/`,
+            languages: locales.reduce(
+                (prev, { code }) => ({
+                    ...prev,
+                    [code]: `https://${shop.domain}/${code}/blog/${handle}/`
+                }),
+                {}
+            )
+        },
+        openGraph: {
+            url: `/blog/${handle}/`,
+            type: 'article',
+            title,
+            description,
+            siteName: shop.name,
+            locale: locale.code,
+            images: []
+        }
+    };
 }
 
 export default async function ArticlePage({
@@ -87,77 +90,14 @@ export default async function ArticlePage({
         notFound();
     }
 
+    const shop = await Shop.findByDomain(domain, { sensitiveData: true });
+    const locale = Locale.from(localeData);
+
+    const api = await ShopifyApolloApiClient({ shop, locale });
+
+    let article: Article;
     try {
-        const shop = await Shop.findByDomain(domain, { sensitiveData: true });
-        const locale = Locale.from(localeData);
-
-        const api = await ShopifyApolloApiClient({ shop, locale });
-        const article = await BlogArticleApi({ api, blogHandle: 'news', handle });
-
-        const jsonLd: WithContext<Article> = {
-            '@context': 'https://schema.org',
-            '@type': 'Article',
-            'url': `https://${shop.domain}/${locale.code}/blog/${handle}/`,
-            'headline': title,
-            'text': article.content,
-            'description': article.seo?.description || article.excerpt || '',
-            'articleSection': 'news',
-            'image': [article.image?.url!],
-            'keywords': article.tags.join(', '),
-            'dateCreated': article.publishedAt,
-            'datePublished': article.publishedAt,
-            'author': {
-                '@type': 'Person',
-                'name': article.authorV2?.name!
-            },
-            'publisher': {
-                '@type': 'Organization',
-                'name': shop.name,
-                'logo': {
-                    '@type': 'ImageObject',
-                    'url': shop.icons?.favicon?.src!
-                }
-            }
-        };
-
-        const publishedAt = new Date(article.publishedAt).toLocaleDateString(locale as any, {
-            weekday: 'long',
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric'
-        });
-
-        return (
-            <article className="prose flex flex-col gap-6 md:max-w-[800px]">
-                <header className="flex flex-col gap-2">
-                    <Label
-                        as="div"
-                        className="text-sm font-semibold leading-snug text-gray-600"
-                        suppressHydrationWarning={true}
-                    >
-                        {publishedAt}
-                    </Label>
-
-                    <Heading
-                        titleClassName="text-3xl lg:text-4xl font-semibold leading-tight text-pretty text-black mb-0"
-                        title={article.title}
-                    />
-                </header>
-
-                {article.image?.url ? (
-                    <div
-                        role="presentation"
-                        className="bg-gray h-32 w-full bg-cover bg-center bg-no-repeat object-cover object-center"
-                        style={{ backgroundImage: `url('${article.image.url}')` }}
-                    />
-                ) : null}
-
-                <Content className="max-w-none" html={article.contentHtml} />
-
-                {/* Metadata */}
-                <JsonLd data={jsonLd} />
-            </article>
-        );
+        article = await BlogArticleApi({ api, blogHandle: 'news', handle });
     } catch (error: unknown) {
         if (Error.isNotFound(error)) {
             notFound();
@@ -167,4 +107,69 @@ export default async function ArticlePage({
         unstable_rethrow(error);
         throw error;
     }
+
+    const jsonLd: WithContext<LdArticle> = {
+        '@context': 'https://schema.org',
+        '@type': 'Article',
+        'url': `https://${shop.domain}/${locale.code}/blog/${handle}/`,
+        'headline': title,
+        'text': article.content,
+        'description': article.seo?.description || article.excerpt || '',
+        'articleSection': 'news',
+        'image': [article.image?.url!],
+        'keywords': article.tags.join(', '),
+        'dateCreated': article.publishedAt,
+        'datePublished': article.publishedAt,
+        'author': {
+            '@type': 'Person',
+            'name': article.authorV2?.name!
+        },
+        'publisher': {
+            '@type': 'Organization',
+            'name': shop.name,
+            'logo': {
+                '@type': 'ImageObject',
+                'url': shop.icons?.favicon?.src!
+            }
+        }
+    };
+
+    const publishedAt = new Date(article.publishedAt).toLocaleDateString(locale as any, {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    });
+
+    return (
+        <article className="prose flex flex-col gap-6 md:max-w-[800px]">
+            <header className="flex flex-col gap-2">
+                <Label
+                    as="div"
+                    className="text-sm font-semibold leading-snug text-gray-600"
+                    suppressHydrationWarning={true}
+                >
+                    {publishedAt}
+                </Label>
+
+                <Heading
+                    titleClassName="text-3xl lg:text-4xl font-semibold leading-tight text-pretty text-black mb-0"
+                    title={article.title}
+                />
+            </header>
+
+            {article.image?.url ? (
+                <div
+                    role="presentation"
+                    className="bg-gray h-32 w-full bg-cover bg-center bg-no-repeat object-cover object-center"
+                    style={{ backgroundImage: `url('${article.image.url}')` }}
+                />
+            ) : null}
+
+            <Content className="max-w-none" html={article.contentHtml} />
+
+            {/* Metadata */}
+            <JsonLd data={jsonLd} />
+        </article>
+    );
 }
