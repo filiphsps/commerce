6de58670ff1361c7bf6e1a9fbef962e932de208a
Filiph Siitam Sandström <filiph@nordcom.io>
2024-09-29 08:36:05 +0200

    Admin: HOTFIX.

--- apps/admin/src/app/layout.tsx
+++ apps/admin/src/app/layout.tsx
@@ -46,8 +46,8 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
     return (
         <html lang="en" className={cn(primaryFont.className, primaryFont.variable, GeistMono.variable)}>
             <head>
-                <meta name="apple-mobile-web-app-capable" content="yes">
-                <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+                <meta name="apple-mobile-web-app-capable" content="yes"/>
+                <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
             </head>
             <body className="group/body grid min-h-[100svh] grid-cols-[100%] grid-rows-1 overflow-x-hidden overscroll-x-none font-sans">
                 <Providers>{children}</Providers>
