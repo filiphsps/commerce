4247e2c8733701507a38a599d9300d64679e11d9
Filiph Siitam Sandström <filfat@hotmail.se>
2026-05-11 20:23:55 +0200

    test(storefront): add fixtures barrel

---
+++ apps/storefront/src/utils/test/fixtures/index.ts
@@ -0,0 +1,6 @@
+export * from './shop';
+export * from './locale';
+export { mockProduct } from './shopify/product';
+export { mockCollection } from './shopify/collection';
+export { mockPage } from './shopify/page';
+export { mockNavigation } from './shopify/navigation';
