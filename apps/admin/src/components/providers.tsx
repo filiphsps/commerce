a91ed11c8b032e60ce0d9010f2ba197aa4dead56
Filiph Sandström <filfat@hotmail.se>
2026-05-13 01:21:27 +0200

    fix: admin: Revert NordstarProvider class.

--- apps/admin/src/components/providers.tsx
+++ apps/admin/src/components/providers.tsx
@@ -23,7 +23,7 @@ export function Providers({ children }: ProvidersProps) {
     }, []);
 
     return (
-        <NordstarProvider theme={Theme} className="block">
+        <NordstarProvider theme={Theme}>
             <Toaster theme="dark" />
 
             {/* `NextTopLoader`, `HeaderProvider`, and `GoogleTagManager` all read */}
