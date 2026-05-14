1cba787beddbf2d93dbcb4b0c27860d2adb4b2b7
Filiph Siitam Sandström <filfat@hotmail.se>
2026-05-14 04:21:11 +0200

    feat(@tagtree/payload): scaffold package skeleton

---
+++ packages/tagtree-payload/vite.config.ts
@@ -0,0 +1,31 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { codecovVitePlugin } from '@codecov/vite-plugin';
+import { defineConfig, mergeConfig } from 'vite';
+
+import base from '../vite.config';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const name = '@tagtree/payload';
+
+export default mergeConfig(
+    base,
+    defineConfig({
+        root: resolve(__dirname),
+        build: {
+            rollupOptions: {
+                output: {
+                    name,
+                },
+            },
+        },
+        plugins: [
+            codecovVitePlugin({
+                enableBundleAnalysis: !!process.env.CODECOV_TOKEN,
+                bundleName: name,
+                uploadToken: process.env.CODECOV_TOKEN,
+            }),
+        ],
+    }),
+);
