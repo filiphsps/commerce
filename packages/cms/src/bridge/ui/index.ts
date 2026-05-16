57131382d74efafacd70147aea302c5408013f56
Filiph Siitam Sandström <filfat@hotmail.se>
2026-05-16 09:45:58 +0200

    feat(cms): <BridgeEditPage> server component

--- packages/cms/src/bridge/ui/index.ts
+++ packages/cms/src/bridge/ui/index.ts
@@ -1,2 +1,3 @@
+export { BridgeEditPage, type BridgeEditPageProps } from './bridge-edit-page';
 export { BridgeFields, type BridgeFieldsProps } from './bridge-fields';
 export { BridgeFormToolbar, type BridgeFormToolbarProps } from './bridge-form-toolbar';
