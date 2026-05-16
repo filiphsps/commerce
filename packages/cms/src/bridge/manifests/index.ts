1b9ae46558a6e58897859d571b30fc90482b80b1
Filiph Siitam Sandström <filfat@hotmail.se>
2026-05-16 21:49:43 +0200

    chore(cms): delete bridge subsystem + drop bridge subpath exports

--- packages/cms/src/bridge/manifests/index.ts
+++
@@ -1,6 +0,0 @@
-import type { BridgeManifest } from '../manifest';
-import { shopBridge } from './shop';
-
-export { shopBridge, stripCommerceProviderSecrets } from './shop';
-
-export const defaultManifests: readonly BridgeManifest[] = [shopBridge as unknown as BridgeManifest];
