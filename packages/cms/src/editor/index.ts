b0f3543f02c7c695b193a66831102c1e26f63494
Filiph Siitam Sandström <filfat@hotmail.se>
2026-05-16 19:38:43 +0200

    feat(cms): editor public re-exports + ui barrel

--- packages/cms/src/editor/index.ts
+++ packages/cms/src/editor/index.ts
@@ -1,3 +1,25 @@
-// Public exports for the @nordcom/commerce-cms/editor subpath.
-// Populated incrementally by subsequent tasks.
-export {};
+export { adminOnly, editorOrAdmin, tenantMember } from './access';
+export { createCollectionEditorActions, type EditorActions } from './actions';
+export { parseFormPayload, pickByFieldNames } from './form-payload';
+export {
+    type CollectionEditorManifest,
+    defineCollectionEditor,
+    type EditorAccess,
+    type EditorAccessCtx,
+    type EditorListColumn,
+} from './manifest';
+export {
+    type RevalidateForManifestArgs,
+    revalidateForManifest,
+    tenantWhere,
+} from './revalidate';
+export type {
+    AuthedPayloadCtx,
+    AuthedUser,
+    BuildFormStateArgs,
+    CollectionTableShellProps,
+    DocumentFormShellProps,
+    EditorRuntime,
+    EditorToolbarShellProps,
+    WithRuntime,
+} from './runtime';
---
+++ packages/cms/src/editor/ui/index.ts
@@ -0,0 +1,6 @@
+export { EditorEditPage, type EditorEditPageProps } from './editor-edit-page';
+export { EditorFields, type EditorFieldsProps } from './editor-fields';
+export { EditorFormToolbar, type EditorFormToolbarProps } from './editor-form-toolbar';
+export { EditorListPage, type EditorListPageProps } from './editor-list-page';
+export { EditorNewPage, type EditorNewPageProps } from './editor-new-page';
+export { EditorVersionsPage, type EditorVersionsPageProps } from './editor-versions-page';
