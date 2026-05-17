bc3180d556a0dc5618560923d5e77487254d8bb5
Filiph Sandström <filfat@hotmail.se>
2026-05-17 02:30:09 +0200

    fix(db): Filter empty shops.

--- packages/db/src/services/shop.ts
+++ packages/db/src/services/shop.ts
@@ -99,7 +99,8 @@ export class ShopService extends Service<ShopBase, typeof ShopModel> {
             limit: 0,
             overrideAccess: true,
         });
-        return docs.map((doc) => docToOnlineShop(doc as unknown as Record<string, unknown>));
+        // Make sure we filter out empty/invalid domains.
+        return docs.filter(d => d).map((doc) => docToOnlineShop(doc as unknown as Record<string, unknown>));
     }
 }
 
