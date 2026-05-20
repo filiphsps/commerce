753aacb3cc465d0a0e95fa36ddb7c779e23ed202
Filiph Siitam Sandström <filfat@hotmail.se>
2026-05-20 06:09:49 +0200

    feat(react-payment-brand-icons): public entrypoint re-exports

---
+++ packages/react-payment-brand-icons/src/index.ts
@@ -0,0 +1,12 @@
+export * from './generated/index';
+export { manifest } from './generated/manifest';
+export type {
+    PaymentIconAlias,
+    PaymentIconName,
+    PaymentIconNameOrAlias,
+} from './generated/names';
+export type { PaymentIconWrapperProps } from './payment-icon';
+export { PaymentIcon } from './payment-icon';
+export type { IconShellProps, PaymentIconChrome, PaymentIconProps } from './shell';
+export { IconShell } from './shell';
+export type { IconManifestEntry } from './types';
